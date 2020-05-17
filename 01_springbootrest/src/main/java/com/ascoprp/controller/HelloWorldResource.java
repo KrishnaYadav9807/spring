b1792fc9fa5ebd8a9f23f70fe594f828cd4be604
Krishna Yadav <krishna.yadav@bottomline.com>
2020-05-17 16:40:14 +0530

    adad

--- 01_springbootrest/src/main/java/com/ascoprp/controller/HelloWorldResource.java
+++ 01_springbootrest/src/main/java/com/ascoprp/controller/HelloWorldResource.java
@@ -16,7 +16,7 @@ public class HelloWorldResource {
 
 	@GetMapping(path = "/hellobean")
 	public HelloBean printHelloBean() {
-		return new HelloBean("Hello Message......");
+		return new HelloBean("Hello Message...sasas...");
 	}
 	
 	// /hellobean/pathvariable/krishna
